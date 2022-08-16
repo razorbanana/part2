9b80971d294c4852ca83cbf9b573ef6c5f0939a0
razorbanana <stanislav.stefyuk2@gmail.com>
2022-08-16 15:01:52 +0300

    Adding courseinfo directory and 2.1 completed

---
+++ courseinfo/src/App.js
@@ -0,0 +1,61 @@
+import React from "react"
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Part = ({part}) => (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+)
+
+const Content = ({parts}) => (
+<>
+  {parts.map(part => 
+    <Part key={part.id} part={part} />
+  )}
+</>
+)
+
+const Total = (props) => (
+<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
